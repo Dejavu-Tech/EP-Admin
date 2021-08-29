7dad99c4c1b826a932edd575b5c15bd38b412ea1
zhrrobert <zhrrobert@hotmail.com>
2021-08-29 21:21:22 +0800

    Add controllers

---
+++ Modules/Home/Controller/NotifyController.class.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * eaterplanet 商城系统
+ *
+ * ==========================================================================
+ * @link      http://www.ch871.com/
+ * @copyright Copyright (c) 2019-2021 ch871.com.
+ * @license   http://www.ch871.com/license.html License
+ * ==========================================================================
+ *
+ * @author    Albert.Z
+ *
+ */
+
+namespace Home\Controller;
+
+class NotifyController extends CommonController {
+	
+	protected function _initialize()
+    {
+    	parent::_initialize();
+	}
+	
+	public function orderNotify()
+    {
+
+        $json_data =  file_get_contents("php://input");
+
+        //log
+
+        $oh = [];
+        $oh['type'] = 'make';
+        $oh['log'] = $json_data;
+		M('log')->add( $oh );
+
+        $data = json_decode( $json_data, true );
+
+        if( !empty($data) && $data['return_code'] == 'success' )
+        {
+            $token = strtolower( $data['token'] );
+            $order_no = $data['order_no'];
+
+            $time = $data['time'];
+            $status = $data['status'];
+            $rider_name = $data['rider_name'];
+            $rider_mobile = $data['rider_mobile'];
+
+			$orderdistribution_order = M('eaterplanet_ecommerce_orderdistribution_order')->where( array('third_order_id' => $order_no ) )->find();
+            $oh = [];
+            $oh['type'] = 'step1';
+            $oh['log'] = !empty($orderdistribution_order) ? 1 : 0;
+            M('log')->add( $oh );
+
+
+            if( !empty($orderdistribution_order) )
+            {
+
+                $appid = D('Home/Front')->get_config_by_name('wepro_appid' );
+                $localtown_mk_token = D('Home/Front')->get_config_by_name('localtown_mk_token' );
+
+                $new_token = strtolower( md5( $appid . $localtown_mk_token ) );
+
+                $order_id = $orderdistribution_order['order_id'];
+
+				$order_info = M('eaterplanet_ecommerce_order')->where( array('order_id' => $order_id ) )->find();
+                $order_num_alias = $order_info['order_num_alias'];
+
+                if( $token == $new_token )
+                {
+                    //校验正常开始处理逻辑
+                    switch( $status )
+                    {
+                        case 'accepted': //抢单了：
+
+
+                            //码科平台回调接口
+                            $other_data = array();
+                            $other_data['data_type'] = 'make';
+                            $other_data['order_status'] = 2;
+                            $other_data['dm_id'] = '';
+                            $other_data['dm_name'] = $rider_name;
+                            $other_data['dm_mobile'] = $rider_mobile;
+                            $other_data['third_id'] = '';
+
+                            //$order_sn, $order_status_id, $other_data
+
+                            D('Seller/Order')->do_localtown_thirth_delivery_return( $order_num_alias,0,$other_data);
+                            break;
+                        case 'wait_to_shop':
+                            //确认前往：+ 前往的日志
+
+                            $oh = array();
+
+                            $oh['order_id']= $order_id;
+
+                            $oh['order_status_id']= 4;
+                            $oh['comment']='配送员'.$rider_name.', 手机  '.$rider_mobile.':确认前往';
+
+                            $oh['date_added']=time();
+                            $oh['notify']=1;
+
+							M('eaterplanet_ecommerce_order_history')->add( $oh );
+                            break;
+                        case 'payed':
+                            //取消订单：+ 前往的日志
+
+                            $oh = array();
+                            $oh['order_id']= $order_id;
+
+                            $oh['order_status_id']= 4;
+                            $oh['comment']='配送员'.$rider_name.', 手机  '.$rider_mobile.':取消订单';
+
+                            $oh['date_added']=time();
+                            $oh['notify']=1;
+
+                            M('eaterplanet_ecommerce_order_history')->add( $oh );
+
+                            break;
+                        case 'geted':
+                            //确认取件  //码科平台回调接口
+
+                            $oh = array();
+                            $oh['order_id']= $order_id;
+
+                            $oh['order_status_id']= 4;
+                            $oh['comment']='配送员'.$rider_name.', 手机  '.$rider_mobile.':确认取件';
+
+                            $oh['date_added']=time();
+                            $oh['notify']=1;
+
+                            M('eaterplanet_ecommerce_order_history')->add( $oh );
+
+
+                            $other_data = array();
+                            $other_data['data_type'] = 'make';
+                            $other_data['order_status'] = 3;
+                            $other_data['dm_id'] = '';
+                            $other_data['dm_name'] = $rider_name;
+                            $other_data['dm_mobile'] = $rider_mobile;
+                            $other_data['third_id'] = '';
+
+                            D('Seller/Order')->do_localtown_thirth_delivery_return( $order_num_alias,0, $other_data );
+                            break;
+                        case 'gotoed':
+                            //确认完成
+                            $oh = array();
+                            $oh['order_id']= $order_id;
+
+                            $oh['order_status_id']= 4;
+                            $oh['comment']='配送员'.$rider_name.', 手机  '.$rider_mobile.':确认送达';
+
+                            $oh['date_added']=time();
+                            $oh['notify']=1;
+
+                            M('eaterplanet_ecommerce_order_history')->add( $oh );
+
+                            $other_data = array();
+                            $other_data['data_type'] = 'make';
+                            $other_data['order_status'] = 4;
+
+                            D('Seller/Order')->do_localtown_thirth_delivery_return( $order_num_alias,6,$other_data);
+
+                            break;
+                    }
+
+                }
+            }
+
+        }
+
+
+        echo 'success';
+
+        //Token加密为模块appid+token md5
+
+        //抢单了：
+        //{"token":"507e9dfa06df88b7655cb2671abe3ccb","order_no":"A20210118440401921","return_code":"success",
+        //"time":1610959924,"status":"accepted","rider_name":"\u5c0f\u72ee","rider_mobile":"15159513836"}
+
+        //确认前往：
+        //{"token":"507e9dfa06df88b7655cb2671abe3ccb","order_no":"A20210118440401921","return_code":"success",
+        //"time":1610959983,"status":"wait_to_shop","rider_name":"\u5c0f\u72ee","rider_mobile":"15159513836"}
+
+        //确认取件
+        //{"token":"507e9dfa06df88b7655cb2671abe3ccb","order_no":"A20210118440401921","return_code":"success",
+        //"time":1610960013,"status":"geted","rider_name":"\u5c0f\u72ee","rider_mobile":"15159513836"}
+
+        //确认完成
+        //{"token":"507e9dfa06df88b7655cb2671abe3ccb","order_no":"A20210118440401921","return_code":"success",
+        //"time":1610960073,"status":"gotoed","rider_name":"\u5c0f\u72ee","rider_mobile":"15159513836"}
+
+
+        /**
+         *
+         * {"token":"24ea86c9748d8a476a7e0519f5ce32c7","order_no":"A20200424875992271",
+         * "return_code":"success","time":1587698827,"status":"gotoed",
+         * "rider_name":"\u6881\u534e\u6587","rider_mobile":"18178110414"}
+         *
+         */
+
+
+    }
+}
---
+++ Modules/Home/Controller/PresalegoodsController.class.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * eaterplanet 商城系统
+ *
+ * ==========================================================================
+ * @link      http://www.ch871.com/
+ * @copyright Copyright (c) 2019-2021 ch871.com.
+ * @license   http://www.ch871.com/license.html License
+ * ==========================================================================
+ *
+ * @author    Albert.Z
+ *
+ */
+
+namespace Home\Controller;
+
+class PresalegoodsController extends CommonController {
+	
+    protected function _initialize()
+    {
+    	parent::_initialize();
+	}
+    public function index(){
+         
+        $presale_index_info = [];
+        //1、顶部幻灯片
+        $slider_list = M('eaterplanet_ecommerce_adv')->where( array('enabled' => 1,'type' => 'presale') )->order('displayorder desc, id desc')->select();
+
+        if(!empty($slider_list))
+        {
+            foreach($slider_list as $key => $val)
+            {
+                $val['image'] = tomedia($val['thumb']);
+                $slider_list[$key] = $val;
+            }
+            $presale_index_info['has_slider'] = 1;
+        }else{
+            $slider_list = array();
+            $presale_index_info['has_slider'] = 0;
+        }
+
+        $presale_index_info['slider_list'] = $slider_list;
+        //2、分享标题 分享图片
+        $presale_share_title = D('Home/Front')->get_config_by_name('presale_share_title');
+        $presale_share_img = D('Home/Front')->get_config_by_name('presale_share_img');
+        $presale_publish = D('Home/Front')->get_config_by_name('presale_publish');
+
+        $presale_index_info['presale_share_title'] = empty($presale_share_title) ? '' : $presale_share_title;
+        $presale_index_info['presale_share_img'] = empty($presale_share_img) ? '' : tomedia($presale_share_img);
+        $presale_index_info['presale_publish'] = empty($presale_publish) ? '' : htmlspecialchars_decode($presale_publish);
+
+        //presale_layout 布局 0 左右布局， 1 横向布局
+        $presale_layout = D('Home/Front')->get_config_by_name('presale_layout');
+
+        $presale_layout = empty($presale_layout) ? 0 : 1;
+        $presale_index_info['presale_layout'] = $presale_layout;
+
+
+        echo json_encode( ['code' => 0, 'data' => $presale_index_info ] );
+        die();
+    }
+
+    /**
+     * @author yj
+     * @desc 获取商品列表有分页
+     */
+    public function load_goods_list()
+    {
+        $result = D('Home/PresaleGoods')->getIndexPresaleGoods(0);
+
+        echo json_encode( $result );
+        die();
+    }
+
+
+    
+}
