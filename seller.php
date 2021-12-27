f352ebf6ec23e9be0b155ee1746adeb29d8d1b5a
zhrrobert <zhrrobert@hotmail.com>
2021-12-28 01:43:20 +0800

    Update seller.php
    fixed some browser error

--- seller.php
+++ seller.php
@@ -13,7 +13,7 @@
  */
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
-header("Content-Type:text/html; charset=utf-8");
+header( "Content-Type:text/html; charset=utf-8");
 
 define('APP_DEBUG', true);
 
