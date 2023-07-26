c6976d7670ec531be4ae8e1e8f1fa3ce7eb8e6e1
Albert.Z <zhrrobert@hotmail.com>
2023-07-26 22:17:31 +0800

    Update tags.php

--- Modules/Admin/Conf/tags.php
+++ Modules/Admin/Conf/tags.php
@@ -1,8 +1,7 @@
-<?php 
-return array(    
- // 添加下面一行定义即可     
- 'app_begin' => array('Behavior\CheckLangBehavior'),   
+<?php
+return array(
+ // 添加下面一行定义即可
+ 'app_begin' => array('Behavior\CheckLangBehavior'),
 
 );
-  
-?>
+
